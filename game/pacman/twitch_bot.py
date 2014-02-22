4ff5ff774915aaccd40befab2d3d21ffd0cfa4b4
Ronald Ning <the@loude.st>
2014-02-22 12:53:42 -0800

    pacman with thread to connect

--- game/pacman/twitch_bot.py
+++ game/pacman/twitch_bot.py
@@ -11,7 +11,7 @@ class twitch_bot(Thread):
 
       HOST="irc.twitch.tv"
       PORT=6667
-      NICK="not_lying"
+      NICK="not_lying"+random.choice(string.letters)+random.choice(string.letters)+random.choice(string.letters)
       IDENT="pacman"    
       REALNAME="Twitch plays pacman"
       CHANNEL="#pacman"
