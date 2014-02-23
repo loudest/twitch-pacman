712680af35398b17ba94193f500f1652f8dac95a
Ronald Ning <the@loude.st>
2014-02-22 18:21:49 -0800

    twitter bot

--- game/pacman/twitter_bot.py
+++ game/pacman/twitter_bot.py
@@ -1,6 +1,11 @@
-import twitter, json
+import twitter, json, memcache
+from threading import Thread
 
 class twitter_bot():
+
+	def __init__(self):
+		#Thread.__init__(self)
+		self.r = memcache.Client(['twitch-plays-pacman.yjvmq8.cfg.usw2.cache.amazonaws.com:11211'], debug=0)
 
 	def searchTweets(self, query):
 		consumer_key = 'ugD4ZKV6GxeLJuiHyosw'
@@ -9,11 +14,18 @@ class twitter_bot():
 		access_token_secret = 'fpuWA5O53hNHtYF4jgIvJ4uEugwOOcWEAVSEaIuwkztLb'
 		api = twitter.Api(	consumer_key, consumer_secret, access_token, access_token_secret)
 		statuses = api.GetSearch(query)
-		one = statuses[0]
+
 		for status in statuses:
+			id = str(status.GetId())
 			user = status.GetUser()
 			text = status.GetText()
-			print "@%s: %s" % (user.GetScreenName(), text)
+			value = self.r.get(id);
+			print "id: ".value."\n"
+			if(value is None): 
+				print "Storing @%s: %s" % (user.GetScreenName(), text)
+				self.r.set(id,'a')
+			else:
+				print "Stored: @%s: %s" % (user.GetScreenName(), text)
 
 t = twitter_bot()
 t.searchTweets('#twitchispacman')
