a47cdcfde5cab835020b04d56c041323d45523d1
Ronald Ning <the@loude.st>
2014-02-22 18:49:05 -0800

    blinky commands

--- game/pacman/twitch_bot.py
+++ game/pacman/twitch_bot.py
@@ -17,7 +17,9 @@ class twitch_bot(Thread):
       NICK="adfadfadfadfadf"
       IDENT="pacman"    
       REALNAME="Twitch plays pacman"
-      CHANNEL="#twitchispacman"
+      CHANNEL_ONE="#twitchispacman"
+      CHANNEL_TWO="#twitchisblinky"
+
 
       readbuffer = ""
       irc=socket.socket()
@@ -25,7 +27,8 @@ class twitch_bot(Thread):
       irc.send("PASS oauth:a7k5b9vzxor9d6tzarlia9rjw2c24kn\r\n")
       irc.send("NICK %s\r\n" % NICK)
       irc.send("USER %s %s :%s\r\n" % (IDENT, IDENT, REALNAME))
-      irc.send("JOIN %s\r\n" % (CHANNEL))
+      irc.send("JOIN %s\r\n" % (CHANNEL_ONE))
+      irc.send("JOIN %s\r\n" % (CHANNEL_TWO))
 
       while True: 
          data = irc.recv(1024)
@@ -38,18 +41,31 @@ class twitch_bot(Thread):
             # TODO: Differentiate PacMan from Ghost
             command = data.split()[3].lower()
             print "COMMAND: %s\n" % (command) 
-            if data.find ('right') != -1:
+            if (data.find ('right') and data.find(CHANNEL_ONE)) != -1:
                self.game.input(self.players[0], self.level, 'd')
                print "PAC-MAN RIGHT\n"
-            if data.find ('left') != -1:
+            if (data.find ('left') and data.find(CHANNEL_ONE)) != -1:
                self.game.input(self.players[0], self.level, 'a')
                print "PAC-MAN LEFT\n"
-            if data.find ('up') != -1:
+            if (data.find ('up') and data.find(CHANNEL_ONE)) != -1:
                self.game.input(self.players[0], self.level, 'w')
                print "PAC-MAN UP\n"
-            if data.find ('down') != -1:
+            if (data.find ('down') and data.find(CHANNEL_ONE)) != -1:
                self.game.input(self.players[0], self.level, 's')
-               print "PAC-MAN DOWN\n"            
+               print "PAC-MAN DOWN\n"
+
+            if (data.find ('right') and data.find(CHANNEL_TWO)) != -1:
+               self.game.input(self.players[1], self.level, 'd')
+               print "BLINKY RIGHT\n"
+            if (data.find ('left') and data.find(CHANNEL_TWO)) != -1:
+               self.game.input(self.players[1], self.level, 'a')
+               print "BLINKY LEFT\n"
+            if (data.find ('up') and data.find(CHANNEL_TWO)) != -1:
+               self.game.input(self.players[1], self.level, 'w')
+               print "BLINKY UP\n"
+            if (data.find ('down') and data.find(CHANNEL_TWO)) != -1:
+               self.game.input(self.players[1], self.level, 's')
+               print "BLINKY DOWN\n"                              
          print data
 
    def run(self):
